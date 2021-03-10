8d8b8df4efe2599cde0ccac4c7164a122e159a6e
WX-DongXing <xingdong.com@live.com>
2021-03-10 22:48:18 +0800

    feat: compatible with methods or function expressions

--- index.js
+++ index.js
@@ -5,6 +5,150 @@ module.exports = function ({ types: t }, option) {
     enable = true
   } = option || {}
   if (!enable) return {}
+
+  /**
+   * get comment description message
+   * @param path
+   * @returns {string}
+   */
+  function getDesc (path) {
+    return path.get('leadingComments').reduce((acc, cur) => {
+      if (/@(?:desc|func)/.test(cur.node.value)) {
+        const value = cur.node.value.replace(/(?:[\s\S]*@(?:desc|func)\s+)(.+)(?:[\S\s]+)?/, '$1')
+        acc += value
+      }
+      return acc
+    }, '')
+  }
+
+  /**
+   * get inside comments
+   * @param body
+   * @returns {*}
+   */
+  function getInsideComments (body) {
+    return body.filter(item => {
+      const leadingComments = item.get('leadingComments')
+      if (leadingComments && leadingComments.length) {
+        return leadingComments.some(comment => /@inner/.test(comment.node.value))
+      } else {
+        return false
+      }
+    }).flatMap(item => item.get('leadingComments'))
+  }
+
+  /**
+   * get format comments
+   * @param path
+   * @param variableDeclarator
+   */
+  function getFormatComments (path, variableDeclarator) {
+    const desc = getDesc(path)
+    const parentComment = { desc, children: [] }
+    let insideComments = []
+    if (['FunctionDeclaration', 'ObjectMethod'].includes(path.node.type)) {
+      const { name, loc } = path.node.id || path.node.key
+      Object.assign(parentComment, { name, loc })
+      const body = path.get('body').get('body')
+      if (body && body.length) {
+        insideComments = getInsideComments(body)
+      }
+    } else if (path.node.type === 'VariableDeclaration') {
+      const { name, loc } = variableDeclarator.get('id')
+      Object.assign(parentComment, { name, loc })
+      const body = variableDeclarator.get('init').get('body').get('body')
+      if (body && body.length) {
+        insideComments = getInsideComments(body)
+      }
+    }
+    if (insideComments && insideComments.length > 0) {
+      parentComment.children = insideComments.map(insideComment => {
+        const { value, loc } = insideComment.node
+        return {
+          loc,
+          value: value.replace(/(?:[\s\S]*@inner\s+)(.+)/, '$1')
+        }
+      })
+    }
+    return parentComment
+  }
+
+  /**
+   * get expression
+   * @param comments
+   * @returns {*}
+   */
+  function getExpression (comments) {
+    return t.expressionStatement(
+      t.logicalExpression(
+        '&&',
+        t.logicalExpression(
+          '&&',
+          t.memberExpression(
+            t.identifier('window'),
+            t.identifier(nameSpace)
+          ),
+          t.binaryExpression(
+            '===',
+            t.memberExpression(
+              t.memberExpression(
+                t.identifier('window'),
+                t.identifier(nameSpace)
+              ),
+              t.identifier('readyState')
+            ),
+            t.numericLiteral(1)
+          )
+        ),
+        t.callExpression(
+          t.memberExpression(
+            t.memberExpression(
+              t.identifier('window'),
+              t.identifier(nameSpace)
+            ),
+            t.identifier('send')
+          ), [
+            t.stringLiteral('RECORD|' + JSON.stringify(comments))
+          ]
+        )
+      )
+    )
+  }
+
+  /**
+   * function handler
+   * @param path
+   */
+  const functionHandler = (path) => {
+    const leadingComments = path.get('leadingComments')
+    if (leadingComments && leadingComments.length) {
+      const comments = getFormatComments(path, path.node.body.body)
+      const expression = getExpression(comments)
+      path.get('body').unshiftContainer('body', expression)
+    }
+  }
+
+  /**
+   * function expression handler
+   * @param path
+   */
+  const expressionHandler = (path) => {
+    const leadingComments = path.get('leadingComments')
+    const declarations = path.get('declarations')
+    if (leadingComments && declarations) {
+      const variableDeclarator = declarations[0]
+      const init = variableDeclarator.get('init')
+      if (
+        variableDeclarator && init &&
+        ['FunctionExpression', 'ArrowFunctionExpression'].includes(init.node.type)
+      ) {
+        const comments = getFormatComments(path, variableDeclarator)
+        const expression = getExpression(comments)
+        init.get('body').unshiftContainer('body', expression)
+      }
+    }
+  }
+
   return {
     visitor: {
       Program: {
@@ -33,68 +177,9 @@ module.exports = function ({ types: t }, option) {
           ))
         }
       },
-      FunctionDeclaration (path) {
-        if (path.node.leadingComments) {
-          const desc = path.node.leadingComments.reduce((acc, cur) => {
-            if (/@(?:desc|func)/.test(cur.value)) {
-              const value = cur.value.replace(/(?:[\s\S]*@(?:desc|func)\s+)(.+)(?:[\S\s]+)/, '$1')
-              acc += value
-            }
-            return acc
-          }, '')
-          const { name, loc } = path.node.id
-          const funcElement = { name, desc, loc, children: [] }
-          if (path.node.body) {
-            const insideComments = path.node.body.body.filter(item => {
-              if (item.leadingComments) {
-                return item.leadingComments.some(comment => /@inner/.test(comment.value))
-              } else {
-                return false
-              }
-            }).flatMap(item => item.leadingComments)
-            if (insideComments && insideComments.length > 0) {
-              funcElement.children = insideComments.map(({ value, loc }) => ({
-                loc,
-                value: value.replace(/(?:[\s\S]*@inner\s+)(.+)/, '$1')
-              }))
-            }
-          }
-          path.get('body').unshiftContainer('body', t.expressionStatement(
-            t.logicalExpression(
-              '&&',
-              t.logicalExpression(
-                '&&',
-                t.memberExpression(
-                  t.identifier('window'),
-                  t.identifier(nameSpace)
-                ),
-                t.binaryExpression(
-                  '===',
-                  t.memberExpression(
-                    t.memberExpression(
-                      t.identifier('window'),
-                      t.identifier(nameSpace)
-                    ),
-                    t.identifier('readyState')
-                  ),
-                  t.numericLiteral(1)
-                )
-              ),
-              t.callExpression(
-                t.memberExpression(
-                  t.memberExpression(
-                    t.identifier('window'),
-                    t.identifier(nameSpace)
-                  ),
-                  t.identifier('send')
-                ), [
-                  t.stringLiteral('RECORD|' + JSON.stringify(funcElement))
-                ]
-              )
-            )
-          ))
-        }
-      }
+      FunctionDeclaration: functionHandler,
+      ObjectMethod: functionHandler,
+      VariableDeclaration: expressionHandler
     }
   }
 }
